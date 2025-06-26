8cfac63b518b31cec6a59dc454dd017f6548a6ed
lunavelez06 <lunava0505@gmail.com>
2025-06-26 13:17:41 -0500

    Navbar
    Se pasó el navbar del index general a la plantilla del navbar

---
+++ FluffyBakery/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,23 @@
+<div class="flex bg-pink-300 h-20 items-center">
+        <div class="">
+            <div class="flex justify-between space-x-2">
+                <div class="">
+                    <div class="text-lg text-white font-semibold">
+                        <a class="" href="">Ayuda</a>
+                        <span class="">|</span>
+                        <a class="" href="">Soporte</a>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="text-white">
+                        <a class="" href="https://www.instagram.com/fluffybakeryshop?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a class="" href="">
+                            <i class="fas fa-user"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
